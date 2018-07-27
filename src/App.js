cf549f4c9ed24878ea0bab95ad85e2e2ec68f3b6
Igar Ramaddhan <igarramaddhan@gmail.com>
2018-07-27 14:24:51 +0700

    fix image background

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, withRouter } from 'react-router-dom';
 import './App.css';
+import Background from './assets/main_bg.png';
 
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
@@ -9,8 +10,21 @@ class MainRouter extends Component {
 	render() {
 		return (
 			<div className="container">
-				<Route exact path={'/'} component={Main} />
-				<Route exact path={'/login'} component={Login} />
+				<img
+					alt="back"
+					src={Background}
+					style={{
+						width: '100%',
+						bottom: 0,
+						right: 0,
+						position: 'fixed'
+						// transform: 'translateX(-50%,-50%)'
+					}}
+				/>
+				<div style={{ zIndex: 100, display: 'flex', flex: 1, width: '100%' }}>
+					<Route exact path={'/'} component={Main} />
+					<Route exact path={'/login'} component={Login} />
+				</div>
 			</div>
 		);
 	}
